4892dcb7fa1257dec0ddd72123a807c301b8f1d8
hekmatbeigverdi <hb8info@gmail.com>
2026-07-16 13:29:13 +0330

    feat(database): add initial SQLite migration

--- apps/desktop/src-tauri/src/lib.rs
+++ apps/desktop/src-tauri/src/lib.rs
@@ -4,12 +4,40 @@ fn greet(name: &str) -> String {
     format!("Hello, {}! You've been greeted from Rust!", name)
 }
 
+use tauri_plugin_sql::{
+    Migration,
+    MigrationKind
+};
+
+const DATABASE_URL: &str = "sqlite:argin-accounting.db";
+
+fn database_migrations() -> Vec<Migration> {
+    vec![
+        Migration {
+            version: 1,
+            description: "initialize_database",
+            sql: include_str!(
+                "../migrations/0001_initialize_database.sql"
+            ),
+            kind: MigrationKind::Up,
+        },
+    ]
+}
+
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_sql::Builder::new().build())
         .plugin(tauri_plugin_opener::init())
         .invoke_handler(tauri::generate_handler![greet])
+        .plugin(
+            tauri_plugin_sql::Builder::default()
+                .add_migrations(
+                    DATABASE_URL,
+                    database_migrations()
+                )
+                .build()
+        )
         .run(tauri::generate_context!())
-        .expect("error while running tauri application");
+        .expect("error while running ArginAccounting");
 }
