95b4df53233904a5bf94d9de596bffd4adf45f1c
Hekmat Beigverdi <hb8info@gmail.com>
2026-07-24 23:03:38 +0330

    feat(audit): add authorizer to audit context

--- packages/audit/src/application/audit/audit-command-context.ts
+++ packages/audit/src/application/audit/audit-command-context.ts
@@ -1,11 +1,13 @@
 import type {
   AuditClock,
   AuditIdGenerator,
+  AuditPermissionAuthorizer,
   AuditRepository
 } from "../../index";
 
 export interface AuditCommandContext {
   idGenerator: AuditIdGenerator;
   clock: AuditClock;
+  authorizer: AuditPermissionAuthorizer;
   auditRepository: AuditRepository;
 }
