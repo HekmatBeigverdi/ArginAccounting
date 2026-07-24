e4439b934069a8fd49907b0b648dd152600a2776
Hekmat Beigverdi <hb8info@gmail.com>
2026-07-24 23:03:28 +0330

    feat(audit): add authorizer to approval context

--- packages/audit/src/application/approval/approval-command-context.ts
+++ packages/audit/src/application/approval/approval-command-context.ts
@@ -2,6 +2,7 @@ import type {
   ApprovalRepository,
   AuditClock,
   AuditIdGenerator,
+  AuditPermissionAuthorizer,
   AuditRepository,
   AuditSource,
   AuditUnitOfWork
@@ -11,6 +12,7 @@ export interface ApprovalCommandContext {
   idGenerator: AuditIdGenerator;
   clock: AuditClock;
   auditSource: AuditSource;
+  authorizer: AuditPermissionAuthorizer;
   unitOfWork: AuditUnitOfWork;
   approvalRepository: ApprovalRepository;
   auditRepository: AuditRepository;
