f2c79b75ff65fc92d74aeda2cb59dee75e255887
Hekmat Beigverdi <hb8info@gmail.com>
2026-07-24 22:31:11 +0330

    feat(audit): add unit of work to approval context

--- packages/audit/src/application/approval/approval-command-context.ts
+++ packages/audit/src/application/approval/approval-command-context.ts
@@ -2,12 +2,16 @@ import type {
   ApprovalRepository,
   AuditClock,
   AuditIdGenerator,
-  AuditRepository
+  AuditRepository,
+  AuditSource,
+  AuditUnitOfWork
 } from "../../index";
 
 export interface ApprovalCommandContext {
   idGenerator: AuditIdGenerator;
   clock: AuditClock;
+  auditSource: AuditSource;
+  unitOfWork: AuditUnitOfWork;
   approvalRepository: ApprovalRepository;
   auditRepository: AuditRepository;
 }
