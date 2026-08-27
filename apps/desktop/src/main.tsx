ae832d79b45be1702145909e6e78f472461db886
Hekmat Beigverdi <hb8info@gmail.com>
2026-08-28 01:22:02 +0330

    fix: install native desktop print bridge

--- apps/desktop/src/main.tsx
+++ apps/desktop/src/main.tsx
@@ -1,6 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import { installNativePrintBridge } from "./platform/install-native-print-bridge";
+
+installNativePrintBridge();
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
