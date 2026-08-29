836bcf344204f3411f464abd4f08308f3c87fc34
Hekmat Beigverdi <hb8info@gmail.com>
2026-08-29 17:56:11 +0330

    feat(party-tauri): export safe sqlite repository

--- packages/party-tauri/src/index.ts
+++ packages/party-tauri/src/index.ts
@@ -1,7 +1,7 @@
 export {
   SqlitePartyDuplicateLookup,
-  SqlitePartyReader,
-  SqlitePartyRepository
+  SqlitePartyReader
 } from "./sqlite-party-store.ts";
 
+export { SqlitePartyRepository } from "./sqlite-party-repository.ts";
 export { SqlitePartyUnitOfWork } from "./sqlite-party-unit-of-work.ts";
