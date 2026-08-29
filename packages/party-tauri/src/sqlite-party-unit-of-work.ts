60f21aae1c9bf0ab4b0926f9a0432ef6a5cce29c
Hekmat Beigverdi <hb8info@gmail.com>
2026-08-29 17:56:17 +0330

    feat(party-tauri): use conflict-mapping repository in unit of work

--- packages/party-tauri/src/sqlite-party-unit-of-work.ts
+++ packages/party-tauri/src/sqlite-party-unit-of-work.ts
@@ -1,7 +1,7 @@
 import type { DatabaseExecutor, DatabaseSession } from "@argin/database";
 import type { PartyUnitOfWork, PartyUnitOfWorkRepositories } from "@argin/party";
 
-import { SqlitePartyRepository } from "./sqlite-party-store.ts";
+import { SqlitePartyRepository } from "./sqlite-party-repository.ts";
 
 function createRepositories(database: DatabaseSession): PartyUnitOfWorkRepositories {
   return Object.freeze({ parties: new SqlitePartyRepository(database) });
