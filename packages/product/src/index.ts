70c0ced4d56198ca97b9d14a36c04c690241eae5
Hekmat Beigverdi <hb8info@gmail.com>
2026-08-30 22:52:30 +0330

    feat: export product lifecycle contracts

--- packages/product/src/index.ts
+++ packages/product/src/index.ts
@@ -1,12 +1,17 @@
 export {
   PRODUCT_DOMAIN_ERROR_CODES,
   ProductDomainError,
+  activateProduct,
+  assignProductCategory,
+  configureProductCapabilities,
   createProduct,
+  deactivateProduct,
   rehydrateProduct,
 } from "./domain/product.ts";
 
 export type {
   CreateProductInput,
+  ProductCapabilities,
   ProductDomainErrorCode,
   ProductKind,
   ProductSnapshot,
