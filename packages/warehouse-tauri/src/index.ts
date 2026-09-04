6f5827c25a3bba7a9bf3c2829fb057496ed3aaa2
Hekmat Beigverdi <hb8info@gmail.com>
2026-09-04 21:53:04 +0330

    feat: export warehouse sqlite adapters

---
+++ packages/warehouse-tauri/src/index.ts
@@ -0,0 +1,7 @@
+export {
+  SqliteWarehouseLocationRepository,
+  SqliteWarehouseRepository,
+  SqliteWarehouseZoneRepository,
+} from "./sqlite-warehouse-repositories.ts";
+
+export { SqliteWarehouseUnitOfWork } from "./sqlite-warehouse-unit-of-work.ts";
