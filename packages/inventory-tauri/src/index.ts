e778a52db79bb78e9391e6c77dd7a1984817639a
Hekmat Beigverdi <hb8info@gmail.com>
2026-09-08 15:59:03 +0330

    feat(inventory): export SQLite Inventory adapters

---
+++ packages/inventory-tauri/src/index.ts
@@ -0,0 +1,10 @@
+export {
+  SqliteInventoryBalanceProjectionRepository,
+  SqliteInventoryBusinessOrderRepository,
+  SqliteInventoryDocumentRepository,
+  SqliteInventoryIdempotencyRepository,
+  SqliteInventoryMovementRepository,
+  SqliteInventoryOpeningBalanceRepository,
+} from "./sqlite-inventory-repositories.ts";
+
+export { SqliteInventoryUnitOfWork } from "./sqlite-inventory-unit-of-work.ts";
