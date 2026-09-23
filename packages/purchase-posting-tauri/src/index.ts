864f0a6f7f9a7a91479cbb0f157ebfb26b9ad01b
Hekmat Beigverdi <hb8info@gmail.com>
2026-09-23 17:43:50 +0330

    feat(phase23): export purchase posting sqlite adapters

---
+++ packages/purchase-posting-tauri/src/index.ts
@@ -0,0 +1,26 @@
+export {
+  SqlitePurchasePostingIdempotencyRepository,
+  SqlitePurchasePostingRepository,
+  SqlitePurchasePostingReversalRepository,
+  SqlitePurchasePostingRuleRepository,
+} from "./sqlite-purchase-posting-repositories.ts";
+export type {
+  PersistedPurchasePostingRule,
+} from "./sqlite-purchase-posting-repositories.ts";
+
+export {
+  SqlitePurchasePostingAccountReader,
+  SqlitePurchasePostingDimensionReader,
+  SqlitePurchasePostingFiscalReader,
+} from "./sqlite-purchase-posting-readers.ts";
+
+export {
+  SqlitePurchasePostingAtomicUnitOfWork,
+  SqlitePurchasePostingReplayUnitOfWork,
+  SqlitePurchasePostingReversalUnitOfWork,
+  SqlitePurchasePostingUnitOfWork,
+} from "./sqlite-purchase-posting-unit-of-work.ts";
+export type {
+  PurchasePostingJournalReverser,
+  SqlitePurchasePostingContext,
+} from "./sqlite-purchase-posting-unit-of-work.ts";
